fed5ae54b60d67024ab02b34185f05b36eeccbec
Max <gievanom@hotmail.com>
2021-03-18 15:36:53 +0100

    Connection to firebase

--- PlantSitter-Website/src/environments/environment.prod.ts
+++ PlantSitter-Website/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCAl32vk_CfI9wVEwg1G4FIlvsZ7O7Vfk",
+    authDomain: "plantsitter-5e5ae.firebaseapp.com",
+    projectId: "plantsitter-5e5ae",
+    storageBucket: "plantsitter-5e5ae.appspot.com",
+    messagingSenderId: "679972165733",
+    appId: "1:679972165733:web:c8f889c6cad12c5c950e8c"
+  }
 };
--- PlantSitter-Website/src/environments/environment.ts
+++ PlantSitter-Website/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBCAl32vk_CfI9wVEwg1G4FIlvsZ7O7Vfk",
+    authDomain: "plantsitter-5e5ae.firebaseapp.com",
+    projectId: "plantsitter-5e5ae",
+    storageBucket: "plantsitter-5e5ae.appspot.com",
+    messagingSenderId: "679972165733",
+    appId: "1:679972165733:web:c8f889c6cad12c5c950e8c"
+  }
 };
 
 /*
