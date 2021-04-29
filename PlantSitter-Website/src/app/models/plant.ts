aa3cefd7967a6b39e3b6875f7ed5312c4fc11818
Leander <leanderleenaerts@hotmail.com>
2021-04-29 12:58:53 +0200

    ADDED Full readout for plants

--- PlantSitter-Website/src/app/models/plant.ts
+++ PlantSitter-Website/src/app/models/plant.ts
@@ -1,7 +1,17 @@
 export interface Plant
 {
-  Name:String;
+  Humidity:String;
   Light_Intensity:String;
+  MaxHumidity:String;
+  MaxLumens:String;
+  MaxSoilHumidity:String;
+  MaxTemperature:String;
+  Message:String;
+  MinHumidity:String;
+  MinLumens:String;
+  MinSoilHumidity:String;
+  MinTemperature:String;
+  Name:String;
   Soil_Humidity:String;
   Temperature:String;
 }
